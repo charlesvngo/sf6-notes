134ded6532c8afd4dafe57a50aff7a21cac55bda
Charles Ngo <charlesngo37@gmail.com>
2023-06-24 17:34:33 -0700

    added tags and layout adjustments

--- src/components/GoalListItem.tsx
+++ src/components/GoalListItem.tsx
@@ -12,14 +12,21 @@ const GoalListItem = () => (
     <Divider orientation="left">LUKE NOTES</Divider>
     <List
       bordered
+      itemLayout="vertical"
+      size="large"
       dataSource={data}
       renderItem={(item) => (
         <List.Item
           actions={[
-            <a key="goal-list-complete">complete</a>,
-            <a key="goal-list-favourite">favourite</a>,
-            <a key="goal-list-edit">edit</a>,
-            <a key="goal-list-delete">delete</a>,
+            <a key="goal-tag-combo">combo</a>,
+            <a key="goal-tag-matchup">matchup</a>,
+            <a key="goal-tag-mechanics">mechanics</a>,
+          ]}
+          extra={[
+            <a key="goal-list-complete">complete </a>,
+            <a key="goal-list-favourite">favourite </a>,
+            <a key="goal-list-edit">edit </a>,
+            <a key="goal-list-delete">delete </a>,
           ]}
         >
           {item}
