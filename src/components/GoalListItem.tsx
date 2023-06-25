b3504329a1c69784a848f8f44733784fdf4adfaa
Charles Ngo <charlesngo37@gmail.com>
2023-06-24 17:48:25 -0700

    updated data structure to resemble what prisma will output

--- src/components/GoalListItem.tsx
+++ src/components/GoalListItem.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import { Divider, List, Typography } from "antd";
 
+// const data = [
+//   "Anti-air with cr.HP or FP DP",
+//   "Buffer Drive Rush with cr.MK",
+//   "React to counterhit cr.MP into cr.HP.",
+// ];
+
 const data = [
-  "Anti-air with cr.HP or FP DP",
-  "Buffer Drive Rush with cr.MK",
-  "React to counterhit cr.MP into cr.HP.",
+  {
+    description: "Anti-air with cr.HP or FP DP",
+    dateCreated: "06/24/23",
+    priority: false,
+    completed: false,
+    tags: ["mechanics", "neutral"],
+  },
+  {
+    description: "Buffer cr.MK with Drive Rush",
+    dateCreated: "06/24/23",
+    priority: false,
+    completed: false,
+    tags: ["mechanics", "neutral"],
+  },
+  {
+    description: "cr.HP triple flash knuckle on punish",
+    dateCreated: "06/24/23",
+    priority: false,
+    completed: false,
+    tags: ["combo"],
+  },
 ];
 
 const GoalListItem = () => (
@@ -17,11 +41,7 @@ const GoalListItem = () => (
       dataSource={data}
       renderItem={(item) => (
         <List.Item
-          actions={[
-            <a key="goal-tag-combo">combo</a>,
-            <a key="goal-tag-matchup">matchup</a>,
-            <a key="goal-tag-mechanics">mechanics</a>,
-          ]}
+          actions={item.tags}
           extra={[
             <a key="goal-list-complete">complete </a>,
             <a key="goal-list-favourite">favourite </a>,
@@ -29,7 +49,7 @@ const GoalListItem = () => (
             <a key="goal-list-delete">delete </a>,
           ]}
         >
-          {item}
+          {item.description}
         </List.Item>
       )}
     />
